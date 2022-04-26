85ee08f28a09c06fc7e2498884a305dff8a08675
piyushhawq <56574565+piyushhawq@users.noreply.github.com>
2022-04-26 21:05:03 +0530

    Ipod

---
+++ src/assets/images/images.js
@@ -0,0 +1,33 @@
+import wallpaper1 from "./pc.jpg";
+import wallpaper2 from "./attack-on-titan.jpg";
+import wallpaper3 from "./death note.jpg";
+import wallpaper4 from "./high-school-dxd.jpg";
+import wallpaper5 from "./jujutsu kisan.jpg";
+import coverflow from "./coverflow.png";
+import games from "./games.jpeg";
+import allsongs from "./allsongs.gif";
+import artists from "./artists.jpg";
+import albums from "./albums.png";
+import song1Img from "./thumb-Excuses.jpg";
+import song2Img from "./thumb-Junoon.jpg";
+import song3Img from "./thumb-Pershawan.jpg";
+import song4Img from "./thumb-Khaab- (AmitMusic.In).jpg";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+	song4Img
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,13 @@
+import music1 from "./Excuses_320(PagalWorld).mp3";
+import music2 from "./Junoon Mitraj_320(PagalWorldl).mp3";
+import music3 from "./Parshawan_320(PagalWorld).mp3";
+import music4 from "./Khaab.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+	music4
+};
+
+export default songs;
